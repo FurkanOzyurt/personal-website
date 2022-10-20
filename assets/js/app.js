450e33f40434bff958ab52c6c82559b6d19cfc20
Furkan Özyurt <48150403+FurkanOzyurt@users.noreply.github.com>
2022-10-20 09:36:46 +0300

    header update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,12 @@ $(document).ready(() => {
     console.log('----------- DOCUMENT LOADED -----------');
     $('.other-information .age').html(new Date().getFullYear() - 2002)
     $('.career-year').html(new Date().getFullYear() - 2018)
+
+
+    $('.open-menu').click(() => {
+        $('header').addClass('active')
+    })
+    $('.close-menu').click(() => {
+        $('header').removeClass('active')
+    })
 })
